1d487a50ddd1bc95770e7616f30f3e582d5ec2ae
Prabhat Kumar <147966138+prabhatkr0@users.noreply.github.com>
2026-06-09 16:57:08 +0530

    Update SpringApplicationContext.java

--- src/main/java/com/webservice/mobile/app/SpringApplicationContext.java
+++ src/main/java/com/webservice/mobile/app/SpringApplicationContext.java
@@ -10,14 +10,7 @@ public class SpringApplicationContext implements ApplicationContextAware {
 
     private static ApplicationContext CONTEXT;
 
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        CONTEXT  = applicationContext;
-    }
-
-    public static Object getBean(String beanName){
-        return CONTEXT.getBean(beanName);
-    }
+    
 
 
 }
